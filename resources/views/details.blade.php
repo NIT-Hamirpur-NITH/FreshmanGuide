4b32ffe9fb548d7b54a32895f174c0e567828483
Param Singh <paramsingh258@gmail.com>
2016-05-30 18:54:14 +0530

    Create blade files and define routes.
    I've defined routes for all four of the basic pages that the site
    will have and created views for them as well. Also created a base
    blade file for the layout of the site.

---
+++ resources/views/details.blade.php
@@ -0,0 +1,9 @@
+<!-- contains content specific to the details page -->
+
+@extends('layouts.base')
+
+@section('title', 'Details')
+
+@section('content')
+    <p> this is the details page </p>
+@endsection
